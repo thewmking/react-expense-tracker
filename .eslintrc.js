488ece109a334cd57b2f216374b8464af102f283
thewmking <thewmking@gmail.com>
2021-04-23 21:56:35 -0400

    Muck about with npm & eslint for way too long

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    'no-console': 0,
+    'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 0,
+    'import/extensions': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/no-unescaped-entities': 0,
+    'no-unused-vars': 1,
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'jsx-a11y',
+  ],
+};
