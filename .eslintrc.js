306f8df25d8524f9b1867d1b2d850b9fc238c360
thewmking <thewmking@gmail.com>
2021-04-23 22:25:45 -0400

    More eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
     'react/prop-types': 0,
     'import/extensions': 0,
+    'import/no-unresolved': 0,
     'react/react-in-jsx-scope': 0,
     'react/no-unescaped-entities': 0,
     'no-unused-vars': 1,
